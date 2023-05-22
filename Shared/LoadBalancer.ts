73aa59dbdac8050e027ad5c018abec3a4a4c94b5
effsnarf <ferenc.somos@gmail.com>
2023-05-22 11:05:28 +0200

    .

--- Shared/LoadBalancer.ts
+++ Shared/LoadBalancer.ts
@@ -242,7 +242,11 @@ class LoadBalancer {
 
     const nodeItem = this.nodeSwitcher.getRandomNode();
 
+    if (request.method == "POST")
+      this.log(`Getting POST data for request ${request.url}`);
     const requestPostData = await Http.getPostData(request);
+    if (request.method == "POST")
+      this.log(`Got POST data for request ${request.url}`);
 
     const incomingItem: IncomingItem = {
       id: this.incomingItemID++, // Unique ID for this request
