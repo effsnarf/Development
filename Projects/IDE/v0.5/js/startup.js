3590da8170c2b3863e7bb0ae932e3cd35b0869e2
effsnarf <ferenc.somos@gmail.com>
2023-06-09 13:16:21 +0200

    .

--- Projects/IDE/v0.5/js/startup.js
+++ Projects/IDE/v0.5/js/startup.js
@@ -282,7 +282,7 @@ document.addEventListener("DOMContentLoaded", async function() {
           const duration = (end - start) / 1000;
           compDom.components.value.push(...this.comps.ide);
           this.comps.ide.forEach(comp => liveData.watch.item("ComponentClasses", comp, { on: { changed: this.onCompChanged } }));
-          await vueUserComponentCompiler.compileAll(this.comps.ide, { fix: false }, (p) => { if (this.$refs.hourglass1) this.$refs.hourglass1.progress = p; });
+          await vueUserComponentCompiler.compileAll(this.comps.ide, { fix: true }, (p) => { if (this.$refs.hourglass1) this.$refs.hourglass1.progress = p; });
           this.$refs.hourglass1.stop();
           this.showHourglass = false;
           this.isIniting = false;
