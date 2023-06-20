4e346576383f5b40e483903d6cc408673f53b651
effsnarf <ferenc.somos@gmail.com>
2023-06-20 15:29:23 +0200

    Deleted folder MemeGenerator (now MG2)

--- Projects/MemeGenerator/MG2.DBP/Node2/start.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-
-call "%~dp0\..\..\..\..\Apps\DatabaseProxy\Server\start.cmd" "%~dp0\..\nodes.config.yaml" "%~dp0\config.yaml" %*
--- Projects/MemeGenerator/MG2.DBP/Proxy/start.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-
-..\..\..\..\Apps\HttpProxy\start.cmd "%~dp0\config.yaml"
