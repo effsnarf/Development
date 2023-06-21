8eaa190aa90d9cd4248896e005e1d2262b6ca41d
Eff <ferenc.somos@gmail.com>
2023-06-20 21:11:56 -0700

    .

--- Projects/MG3/Processes/Generator.Descriptions/app.ts
+++ Projects/MG3/Processes/Generator.Descriptions/app.ts
@@ -1,58 +1,75 @@
 import fs from "fs";
 import "colors";
+import { Loading } from "@shared/Loading";
 import { Configuration } from "@shared/Configuration";
 import { Database } from "@shared/Database/Database";
 import { ChatOpenAI, Roles } from "../../../../Apis/OpenAI/classes/ChatOpenAI";
 
+const wait = (ms: number) => {
+  const loading = Loading.startNew(`Waiting ${ms.unitifyTime()}..`);
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const convertKeysToTitleCase = (obj: any) => {
+  const ret = {} as any;
+  for (const key of Object.keys(obj)) {
+    ret[key[0].toUpperCase() + key.slice(1)] = obj[key];
+  }
+  return ret;
+};
+
 (async () => {
-  const config = (await Configuration.new()).data;
-  const db = await Database.new(config.database.content);
+  while (true) {
+    const config = (await Configuration.new()).data;
+    const db = await Database.new(config.database.content);
 
-  const batchSize = 10;
-  const maxInstancesCount = 100;
+    const batchSize = 10;
+    const maxInstancesCount = 100;
 
-  console.log(`Getting generators..`.gray);
+    console.log(`Getting generators..`.gray);
 
-  const generators = await db.find(
-    "Generators",
-    { Desc: null },
-    { InstancesCount: -1 },
-    batchSize,
-    0,
-    true
-  );
+    const generators = await db.find(
+      "Generators",
+      { Desc: null },
+      { InstancesCount: -1 },
+      batchSize,
+      0,
+      true
+    );
 
-  console.log(`Found ${generators.length} generators`.gray);
+    console.log(`Found ${generators.length} generators`.gray);
 
-  console.log(generators.map((g) => g.displayName.green).join("\n"));
+    console.log(generators.map((g) => g.displayName.green).join("\n"));
 
-  for (const generator of generators) {
-    let instancesCount = maxInstancesCount;
-    const desc = {} as any;
-    while (
-      (instancesCount > 0 && !Object.values(desc).length) ||
-      Object.values(desc).some((a) => !a)
-    ) {
-      try {
-        console.log(`${generator.displayName.green}`);
-        console.log(`Getting ${generator.displayName.green} instances..`.gray);
-        const instances = await db.find(
-          "Instances",
-          { LanguageCode: "en", GeneratorID: generator.generatorID },
-          { TotalVotesScore: -1 },
-          instancesCount,
-          0,
-          true
-        );
-        console.log(`Found ${instances.length} instances`.gray);
+    for (const generator of generators) {
+      let instancesCount = maxInstancesCount;
+      const desc = {} as any;
+      while (
+        (instancesCount > 0 && !Object.values(desc).length) ||
+        Object.values(desc).some((a) => !a)
+      ) {
+        try {
+          console.log(`${generator.displayName.green}`);
+          console.log(
+            `Getting ${generator.displayName.green} instances..`.gray
+          );
+          const instances = await db.find(
+            "Instances",
+            { LanguageCode: "en", GeneratorID: generator.generatorID },
+            { TotalVotesScore: -1 },
+            instancesCount,
+            0,
+            true
+          );
+          console.log(`Found ${instances.length} instances`.gray);
 
-        const tasks = {
-          haiku: "Write a haiku about this meme",
-          poem: "Write a poem exactly 9 verses long about this meme",
-        } as any;
+          const tasks = {
+            haiku: "Write a haiku about this meme",
+            poem: "Write a poem exactly 9 verses long about this meme",
+          } as any;
 
-        for (const task of Object.entries(tasks)) {
-          const message = `${task[1]}
+          for (const task of Object.entries(tasks)) {
+            const message = `${task[1]}
         , based on its name and sayings.
   
         ${generator.displayName}
@@ -61,23 +78,26 @@ import { ChatOpenAI, Roles } from "../../../../Apis/OpenAI/classes/ChatOpenAI";
           [inst.text0, inst.text1].filter((a) => a).join(", ")
         )}
         `;
-          console.log(`Generating ${task[0]}..`.gray);
+            console.log(`Generating ${task[0]}..`.gray);
 
-          const chat = await ChatOpenAI.new(Roles.ChatGPT, false);
-          const reply = await chat.send(message);
+            const chat = await ChatOpenAI.new(Roles.ChatGPT, false);
+            const reply = await chat.send(message);
 
-          console.log(reply);
+            console.log(reply);
 
-          desc[task[0]] = reply;
+            desc[task[0]] = reply;
+          }
+        } catch (ex: any) {
+          instancesCount -= 5;
+          console.log(`Trying again with ${instancesCount} instances..`.gray);
+        } finally {
+          await wait((1).minutes());
         }
-      } catch (ex: any) {
-        instancesCount -= 5;
-        console.log(`Trying again with ${instancesCount} instances..`.gray);
       }
+
+      generator.desc = desc;
+
+      db.upsert("Generators", convertKeysToTitleCase(generator));
     }
-
-    generator.desc = desc;
-
-    db.upsert("Generators", generator);
   }
 })();
