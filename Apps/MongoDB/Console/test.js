fee3d8179f2dc69efddbf6410be82a5c6b08c4b2
effsnarf <ferenc.somos@gmail.com>
2023-07-11 18:10:22 +0200

    /.

---
+++ Apps/MongoDB/Console/test.js
@@ -0,0 +1,15 @@
+
+const adviceAnimals = [
+    "Philosoraptor",
+    "Grumpy Cat",
+    "Forever Alone",
+    "Insanity Wolf",
+    "Pedobear",
+    "Socially Awkward Penguin",
+    "Slowpoke",
+    "Foul Bachelor Frog",
+];
+
+const gens = await dbs.MemeGenerator.find("Generators", { DisplayName: { $in: adviceAnimals } }, {});
+
+await dbs.MG3.upsert("Generators", gens);
