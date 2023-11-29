984e32224cca70b4b1c45b273c197f895c83698e
Eff <ferenc.somos@gmail.com>
2023-11-29 00:29:41 -0800

    .

---
+++ server.startup.cmd
@@ -0,0 +1,5 @@
+start cmd /k "cd /d D:\Development\Projects\MG3\DBP\Proxy & start.cmd"
+start cmd /k "cd /d D:\Development\Projects\MG3\DBP\Node1 & start.cmd"
+start cmd /k "cd /d D:\Development\Projects\MG3\Web\Site\Node1 & start.cmd"
+start cmd /k "cd /d D:\Development\Projects\MG3\Web\Site\Proxy & start.cmd"
+start cmd /k "cd /d D:\Development\Projects\IDE\VueStudio\Web & start.cmd"
