1fbeab247d04c3b4fa4c21427e7a336165ef785b
Eff <ferenc.somos@gmail.com>
2023-12-06 02:16:52 -0800

    .

--- server.startup.cmd
+++ server.startup.cmd
@@ -2,4 +2,7 @@ start cmd /k "cd /d D:\Development\Projects\MG3\DBP\Proxy & start.cmd"
 start cmd /k "cd /d D:\Development\Projects\MG3\DBP\Node1 & start.cmd"
 start cmd /k "cd /d D:\Development\Projects\MG3\Web\Site\Node1 & start.cmd"
 start cmd /k "cd /d D:\Development\Projects\MG3\Web\Site\Proxy & start.cmd"
+start cmd /k "cd /d D:\Development\Projects\MG3\Web\Images & start.cmd"
 start cmd /k "cd /d D:\Development\Projects\IDE\VueStudio\Web & start.cmd"
+
+start cmd /k "cd /d D:\Development\Projects\4chanScraper\App & 4chanScraper.cmd"
