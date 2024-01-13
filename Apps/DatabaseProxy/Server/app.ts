e692bb1d4886fbe24fc795eebe50628bf56e6b22
Eff <ferenc.somos@gmail.com>
2024-01-12 22:18:41 -0800

    .

--- Apps/DatabaseProxy/Server/app.ts
+++ Apps/DatabaseProxy/Server/app.ts
@@ -467,36 +467,6 @@ const loadApiMethods = async (db: MongoDatabase, config: any) => {
     }
     // #endregion
 
-    httpServer.all(
-      "/shakespearize",
-      processRequest(async (req: any, res: any, postData: any) => {
-        if (!postData?.text)
-          return res.end(
-            JSON.stringify(`Usage: /shakespearize (POST { text: "..." })`)
-          );
-
-        const sheakspearize = async (text: string) => {
-          const url = `http://95.183.1.58/shakespearize`;
-          var response = await fetch(url, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ text: text }),
-          });
-
-          var result = await response.json();
-          return result;
-        };
-
-        const text = postData.text;
-
-        const result = await sheakspearize(text);
-
-        return res.end(JSON.stringify(result));
-      })
-    );
-
     // #region 📝 File Transpilation
     httpServer.get(
       "/*.js",
