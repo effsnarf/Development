6daf974e188474dc5fd9ecf500f75f53d53594da
eff <ferenc.somos@gmail.com>
2024-01-13 07:20:29 +0100

    .

--- Apps/HttpProxy/app.ts
+++ Apps/HttpProxy/app.ts
@@ -188,15 +188,17 @@ class TaskManager {
       const text = postData.text;
       const sheakspearize = async (text: string) => {
         const url = `http://10.35.16.38/shakespearize`;
-        var response = await fetch(url, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ text: text }),
-        });
 
-        var result = await response.json();
+        // var response = await fetch(url, {
+        //   method: "POST",
+        //   headers: {
+        //     "Content-Type": "application/json",
+        //   },
+        //   body: JSON.stringify({ text: text }),
+        // });
+
+        // var result = await response.json();
+        const result = { text, shakespearized: `To be or not to be.` };
         return result;
       };
       const result = await sheakspearize(text);
