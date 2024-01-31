35ed0c9cec7e1439b8471e9ef13e579f4707a483
eff <ferenc.somos@gmail.com>
2024-01-31 15:15:40 +0100

    .

---
+++ External/falcon-7b/download.py
@@ -0,0 +1,6 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_name = "tiiuae/falcon-7b"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
