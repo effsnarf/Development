142dd5fd98c9677c4b278948369a48bf96e9727a
eff <ferenc.somos@gmail.com>
2024-09-09 18:48:26 +0300

    animations

--- Projects/MemeGenerator/MG4/MgMobile/Nuxt/components/UiList.vue
+++ Projects/MemeGenerator/MG4/MgMobile/Nuxt/components/UiList.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 div("class"="comp-ui-list")
-  div("v-for"="i in 10")
+  div("v-for"="i in 10", "class"="comp-ui-list-item", "@click"="onClickListItem")
     slot
 </template>
 
@@ -10,6 +10,12 @@ export default {
   props: {
     items: {
       default: null,
+    },
+  },
+  methods: {
+    onClickListItem: function (e) {
+      const listItem = e.target.closest(".comp-ui-list-item");
+      listItem.classList.add("selected-active");
     },
   },
 };
