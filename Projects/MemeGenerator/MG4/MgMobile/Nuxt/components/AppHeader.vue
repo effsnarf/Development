b644487143ddf49139eb84fceb5571aa11778dd6
eff <ferenc.somos@gmail.com>
2024-09-13 14:30:44 +0300

    .

---
+++ Projects/MemeGenerator/MG4/MgMobile/Nuxt/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+div("class"="comp-app-header")
+  UiLink(":url"="'/'")
+    div("v-text"="'Meme Generator'", "class"="title")
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style scoped>
+.comp-app-header {
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translate(-50%, 0);
+}
+.title {
+  font-family: impact;
+  font-size: 3vh;
+  text-transform: uppercase;
+  opacity: 1;
+  text-shadow: 0vh -0.1vh 2px white;
+  color: black;
+}
+</style>
